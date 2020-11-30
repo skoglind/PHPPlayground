b8cf1826c77d19b6b995d0b5d65f7db95e09baf8
Fredrik Skoglind <fredrik@fredriks-mbp.lan>
2020-11-30 13:19:05 +0100

    Code Cleanup

--- faculty/index.php
+++ faculty/index.php
@@ -1,31 +1,33 @@
+<?php
+
+	/* Author: Fredrik Skoglind, 2020 */
+
+	// Presets
+	$minvalue = 1;
+	$maxvalue = 9;
+
+	// Get numeric $uservalue from querystring "x", with min/max value
+	if(isset($_GET['x']) && 
+		is_numeric($_GET['x']) &&
+		$_GET['x'] >= $minvalue &&
+		$_GET['x'] <= $maxvalue) {
+		$uservalue = intval($_GET['x']);
+	} else {
+		$uservalue = 1;
+	}
+
+	// Calculate faculty, recursive, n = x * [...] * 2 * 1
+	function calcFaculty($x, $value = 1) : int {
+		if( $x < 2 ) { return $value; } // Base case
+		else { return calcFaculty($x - 1, $value * $x); }
+	}
+
+	// Do the calculations
+	$calculatedValue = calcFaculty($uservalue);
+
+?>
 <?php $projtitle = 'Faculty'; ?>
 <?php require_once( '../_proj_head.php' ) ?>
-	<?php
-		// Presets
-		$minvalue = 1;
-		$maxvalue = 9;
-
-		// Get numeric $uservalue from querystring "x", min: 1, max: 9
-		if(isset($_GET['x']) && 
-			is_numeric($_GET['x']) &&
-			$_GET['x'] >= $minvalue &&
-			$_GET['x'] <= $maxvalue) {
-			$uservalue = intval($_GET['x']);
-		} else {
-			$uservalue = 1;
-		}
-
-		// Calculate faculty, recursive, n = x * [...] * 2 * 1
-		function calcFaculty($x, $value = 1) : int {
-			if( $x < 2 ) { return $value; } // Base case
-			else { return calcFaculty($x - 1, $value * $x); }
-		}
-
-		// Do the calculations
-		$calculatedValue = calcFaculty($uservalue);
-
-	?>
-
 	<p>Calculate: <?php echo $uservalue; ?>! = <?php echo $calculatedValue ?></p>
 	<div class="data">
 		<input type="button" value="-1" onclick="location.href='?x=<?php echo $uservalue - 1 ?>'" <?php if($uservalue-1 < $minvalue){ echo 'disabled'; } ?> />
